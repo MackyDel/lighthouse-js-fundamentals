4b9f5f91bd21290570295128a11eef7b2dbc695c
Mackenzie Delaney <mackenzie.a.delaney@gmail.com>
2023-01-09 12:11:21 -0600

    reversed list of ingredients forloop

--- ingredients.js
+++ ingredients.js
@@ -1,6 +1,4 @@
 const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
-let i = 0;
-while (i < ingredients.length) {
+for (let i = ingredients.length -1; i >= 0; i--) {
   console.log(ingredients[i]);
-  i++;
 }
